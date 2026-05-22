9c602ca654be65c82b61c1b2d63cf023e7954e7a
ayan999-tech <ayanhamdani999@gmail.com>
2026-05-22 16:18:23 +0500

    Revert "some remaing files"
    This reverts commit 8db13707e0ac12f7500c852d6dde2b7bfa9957c3.

--- CASA-Client/obj/Debug/net8.0/CASA-Client.AssemblyInfo.cs
+++ CASA-Client/obj/Debug/net8.0/CASA-Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CASA-Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+184a7a2300efb300fbbc1a4e70e4a9eaddda5d34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e296dccb6e9a27ba72f0925b7f198f3deea47fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("CASA-Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CASA-Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
